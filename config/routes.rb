64d4beb634f48f2bf95c41badcc3b650344b3f5d
Patrick Misch <pat.misch@gmail.com>
2019-01-11 14:44:33 -0500

    added home page and set up route based on auth.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  authenticated :user do 
+    root to: "dashboard#index", as: :user_root
+  end
+
   root to: "home#index"
 end
