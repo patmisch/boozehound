a78de690390761bc092de63ab37080300dc6aa3c
Patrick Misch <pat.misch@gmail.com>
2019-03-30 13:44:33 -0400

    add validlations for new user fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :first_name, :last_name, :username, presence: true
+  validates :username, uniqueness: true
 
   has_many :consumed_drinks
   has_many :drinks, through: :consumed_drinks
