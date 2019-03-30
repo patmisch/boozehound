1cc9a860a5fb723012dd073c08b9b2560eab5eb0
Patrick Misch <pat.misch@gmail.com>
2019-03-30 13:33:29 -0400

    add name and username to user model. update new form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if:  :devise_controller?
 
   private
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :username])
+    end
 
     def authorize_admin
       redirect_to root_path, alert: "Permissions denied" unless
