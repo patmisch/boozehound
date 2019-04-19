f9ce84d6c12275d68137c05eb801a136a486c5c4
Patrick Misch <pat.misch@gmail.com>
2019-04-18 22:13:28 -0400

    add modeling for following/follower with some basic model tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should follow and unfollow a user" do
+    pat = users(:one)
+    steve  = users(:admin)
+    assert_not pat.following?(steve)
+    pat.follow(steve)
+    assert pat.following?(steve)
+    assert steve.followers.include?(pat)
+    pat.unfollow(steve)
+    assert_not pat.following?(steve)
+  end
 end
